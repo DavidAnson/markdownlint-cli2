c70afadd472095d310b84d072ab7cf5f7c4df62c
David Anson <david@dlaa.me>
2021-04-01 20:03:01 -0700

    Revert "Simplify module resolution to remove use of require.resolve.paths() (which is not supported under VS Code)." which breaks when installed globally (fixes #19).
    This reverts commit 37d590865f20e18f0593e6e176f2c4c8951a2821.

--- markdownlint-cli2.js
+++ markdownlint-cli2.js
@@ -57,7 +57,7 @@ const readConfig = (dir, name, otherwise) => {
 // Require a module ID with the specified directory in the path
 const requireResolve = (dir, id) => {
   if (typeof id === "string") {
-    const paths = [ dir ];
+    const paths = [ dir, ...dynamicRequire.resolve.paths("") ];
     const resolved = dynamicRequire.resolve(id, { paths });
     return dynamicRequire(resolved);
   }
