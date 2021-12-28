1f9f99e1de4aa8da5a45e8b063e460ca1341281b
David Anson <david@dlaa.me>
2021-12-28 22:31:47 +0000

    Update dependency: execa to 6.0.0.

--- test/markdownlint-cli2-test-exec.js
+++ test/markdownlint-cli2-test-exec.js
@@ -3,18 +3,21 @@
 "use strict";
 
 const path = require("path");
-const execa = require("execa");
 const testCases = require("./markdownlint-cli2-test-cases");
 
-const invoke = (directory, args, noRequire, env, script) => () => execa.node(
-  path.join(__dirname, "..", script || "markdownlint-cli2.js"),
-  args,
-  {
-    "cwd": directory,
-    "env": env || {},
-    "reject": false,
-    "stripFinalNewline": false
-  }
-);
+const invoke = (directory, args, noRequire, env, script) => async () => {
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax
+  const { execaNode } = await import("execa");
+  return execaNode(
+    path.join(__dirname, "..", script || "markdownlint-cli2.js"),
+    args,
+    {
+      "cwd": directory,
+      "env": env || {},
+      "reject": false,
+      "stripFinalNewline": false
+    }
+  );
+};
 
 testCases("exec", invoke, false, true, true, true);
