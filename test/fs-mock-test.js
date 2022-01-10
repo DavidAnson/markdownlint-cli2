19622e6abec85739a5fd9ae043c48b092c9fb834
David Anson <david@dlaa.me>
2022-01-10 02:23:33 +0000

    Update dependency: ava to 4.0.1.

--- test/fs-mock-test.js
+++ test/fs-mock-test.js
@@ -3,57 +3,54 @@
 "use strict";
 
 const path = require("path");
+const { promisify } = require("util");
 const test = require("ava").default;
 const FsMock = require("./fs-mock");
 
 const mockPath = "/mock";
 const testFile = path.basename(__filename);
 
-test.cb("fsMock.stat", (t) => {
+test("fsMock.stat", async (t) => {
+  t.plan(2);
+  const fs = new FsMock(__dirname);
+  const fsStat = promisify(fs.stat);
+  // @ts-ignore
+  const stat = await fsStat(path.join(mockPath, testFile));
+  t.truthy(stat);
+  t.true(stat.size > 0);
+});
+
+test("fsMock.lstat", async (t) => {
   t.plan(3);
   const fs = new FsMock(__dirname);
-  fs.stat(path.join(mockPath, testFile), (err, stat) => {
-    t.is(err, null);
-    t.truthy(stat);
-    t.true(stat.size > 0);
-    t.end();
-  });
+  const fsLstat = promisify(fs.lstat);
+  // @ts-ignore
+  const stat = await fsLstat(path.join(mockPath, testFile));
+  t.truthy(stat);
+  t.true(stat.size > 0);
+  t.false(stat.isSymbolicLink());
 });
 
-test.cb("fsMock.lstat", (t) => {
-  t.plan(4);
-  const fs = new FsMock(__dirname);
-  fs.lstat(path.join(mockPath, testFile), (err, stat) => {
-    t.is(err, null);
-    t.truthy(stat);
-    t.true(stat.size > 0);
-    t.false(stat.isSymbolicLink());
-    t.end();
-  });
-});
-
-test.cb("fsMock.lstat symbolic links", (t) => {
-  t.plan(4);
+test("fsMock.lstat symbolic links", async (t) => {
+  t.plan(3);
   const fs = new FsMock(__dirname, true);
-  fs.lstat(path.join(mockPath, testFile), (err, stat) => {
-    t.is(err, null);
-    t.truthy(stat);
-    t.true(stat.size > 0);
-    t.true(stat.isSymbolicLink());
-    t.end();
-  });
+  const fsLstat = promisify(fs.lstat);
+  // @ts-ignore
+  const stat = await fsLstat(path.join(mockPath, testFile));
+  t.truthy(stat);
+  t.true(stat.size > 0);
+  t.true(stat.isSymbolicLink());
 });
 
-test.cb("fsMock.readdir", (t) => {
-  t.plan(4);
+test("fsMock.readdir", async (t) => {
+  t.plan(3);
   const fs = new FsMock(__dirname);
-  fs.readdir(mockPath, (err, files) => {
-    t.is(err, null);
-    t.true(Array.isArray(files));
-    t.true(files.length > 0);
-    t.true(files.includes(testFile));
-    t.end();
-  });
+  const fsReaddir = promisify(fs.readdir);
+  // @ts-ignore
+  const files = await fsReaddir(mockPath);
+  t.true(Array.isArray(files));
+  t.true(files.length > 0);
+  t.true(files.includes(testFile));
 });
 
 test("fsMock.promises.*", async (t) => {
