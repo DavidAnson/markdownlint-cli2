d4d4131a36b764af372bdc391affdfa17c68c3a7
David Anson <david@dlaa.me>
2024-08-29 22:33:33 -0700

    Update tests to account for new markdownlint library version.

--- test/tilde-paths-commonjs/scripts/any-blockquote.cjs
+++ test/tilde-paths-commonjs/scripts/any-blockquote.cjs
@@ -2,25 +2,26 @@
 
 "use strict";
 
-const { URL } = require("url");
-const { filterTokens } = require("markdownlint/helpers");
-
+/** @type import("markdownlint").Rule */
 module.exports = {
   "names": [ "any-blockquote" ],
   "description": "Rule that reports an error for any blockquote",
   "information": new URL(
     "https://github.com/DavidAnson/markdownlint" +
-    "/blob/master/test/rules/any-blockquote.js"
+    "/blob/main/test/rules/any-blockquote.js"
   ),
   "tags": [ "test" ],
+  "parser": "markdownit",
   "function": (params, onError) => {
-    filterTokens(params, "blockquote_open", (blockquote) => {
+    const blockquotes = params.parsers.markdownit.tokens.
+      filter((token => token.type === "blockquote_open"));
+    for (const blockquote of blockquotes) {
       const lines = blockquote.map[1] - blockquote.map[0];
       onError({
         "lineNumber": blockquote.lineNumber,
         "detail": "Blockquote spans " + lines + " line(s).",
         "context": blockquote.line.substr(0, 7)
       });
-    });
+    }
   }
 };
--- test/tilde-paths-module/scripts/any-blockquote.mjs
+++ test/tilde-paths-module/scripts/any-blockquote.mjs
@@ -1,25 +1,26 @@
 // @ts-check
 
-import { URL } from "url";
-import { filterTokens } from "markdownlint/helpers";
-
+/** @type import("markdownlint").Rule */
 const rule = {
   "names": [ "any-blockquote" ],
   "description": "Rule that reports an error for any blockquote",
   "information": new URL(
     "https://github.com/DavidAnson/markdownlint" +
-    "/blob/master/test/rules/any-blockquote.js"
+    "/blob/main/test/rules/any-blockquote.js"
   ),
   "tags": [ "test" ],
+  "parser": "markdownit",
   "function": (params, onError) => {
-    filterTokens(params, "blockquote_open", (blockquote) => {
+    const blockquotes = params.parsers.markdownit.tokens.
+      filter((token => token.type === "blockquote_open"));
+    for (const blockquote of blockquotes) {
       const lines = blockquote.map[1] - blockquote.map[0];
       onError({
         "lineNumber": blockquote.lineNumber,
         "detail": "Blockquote spans " + lines + " line(s).",
         "context": blockquote.line.substr(0, 7)
       });
-    });
+    }
   }
 };
 
