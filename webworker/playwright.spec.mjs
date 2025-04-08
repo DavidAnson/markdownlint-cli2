4864127ecbe6a3c2d5711ec1954376e85c4f8297
David Anson <david@dlaa.me>
2025-04-07 20:56:51 -0700

    Update CI workflow test run-tests-webworker to capture and preserve Playwright QUnit screenshots report for 3 days.

--- webworker/playwright.spec.mjs
+++ webworker/playwright.spec.mjs
@@ -6,6 +6,8 @@ import { testUrl } from "./playwright.shared.mjs";
 const bannerId = "qunit-banner";
 const failClass = "qunit-fail";
 const passClass = "qunit-pass";
+const userAgentId = "qunit-userAgent";
+const userAgentSelector = `#${userAgentId}`;
 const failSelector = `#${bannerId}.${failClass}`;
 const passSelector = `#${bannerId}.${passClass}`;
 
@@ -13,5 +15,9 @@ test("Test site QUnit", async ({ page }) => {
   await page.goto(testUrl);
   const bannerLocator = page.locator(`${passSelector}, ${failSelector}`);
   await bannerLocator.waitFor();
+  const userAgentLocator = page.locator(userAgentSelector);
+  const userAgent = await userAgentLocator.textContent() || "[UNKNOWN]";
+  const path = `playwright-screenshot-${userAgent.replaceAll(/[ ,/:;\\]/gu, "_")}.png`;
+  await page.screenshot({ path, "fullPage": true });
   await expect(bannerLocator).toHaveClass(passClass, { "timeout": 1 });
 });
