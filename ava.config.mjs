fd3b061d8974137baf09f96927d5f266322b55f8
David Anson <david@dlaa.me>
2026-05-17 17:34:14 -0700

    Remove ava.config.mjs (sets "CI" environment variable to true) for no longer having an effect.

--- ava.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-export default {
-  "environmentVariables": {
-    // Emulate GitHub Actions behavior for consistency (see FORCE_COLOR and FORCE_HYPERLINK)
-    // https://docs.github.com/en/actions/reference/workflows-and-actions/variables
-    "CI": "true"
-  }
-};
