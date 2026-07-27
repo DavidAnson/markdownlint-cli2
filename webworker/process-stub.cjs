e4e659ddc65ccef0d59fc70290477d9d04481403
David Anson <david@dlaa.me>
2026-07-26 22:07:00 -0700

    Add a stub for process.nextTick (using queueMicrotask) now that globby (indirectly) invokes it in webworker tests.

--- webworker/process-stub.cjs
+++ webworker/process-stub.cjs
@@ -6,6 +6,9 @@ module.exports = {
   "argv": [],
   "cwd": () => "/",
   "env": {},
+  // eslint-disable-next-line jsdoc/reject-function-type, jsdoc/reject-any-type
+  "nextTick": (/** @type {Function} */ callback, /** @type {any[]} */ ...args) =>
+    queueMicrotask(() => callback(...args)),
   "versions": {
     "node": "0.0"
   }
